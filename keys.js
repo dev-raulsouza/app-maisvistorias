b18d9ebc8aee0a8010df5aa7fe2d022747ec4fd3
dev-raulsouza <105613416+dev-raulsouza@users.noreply.github.com>
2022-05-16 13:58:34 -0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -3,4 +3,6 @@ var keysjson = require('./keys.json');
 
     let accessKeyID = keysjson[access_key];
     let secretAccessKeyID = keysjson[secret_access_key];
+    
+    return {accessKeyID, secretAccessKeyID};
 }
